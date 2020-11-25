c2fe311b69b0d95fd84a036c0c5f5e1e51caaeec
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-25 18:53:17 +0100

    fix: move imports back to ssr/browser files

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,3 +3,22 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+
+import 'normalize.css'
+import 'remark-admonitions/styles/classic.css'
+import 'prismjs/themes/prism.css'
+
+import './styles/themes/default.css'
+import './styles/global.css'
+import './styles/grid.css'
+import './styles/typography.css'
+
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-jsx'
+import 'prismjs/components/prism-tsx'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-pug'
+import 'prismjs/components/prism-go'
+import 'prismjs/components/prism-yaml'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-shell-session'
