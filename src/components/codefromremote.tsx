9c2a479ce446d40815aae38f0d9fd77f0a1d015e
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-25 12:01:31 +0100

    fix: improve line stripping in codefromremote

--- src/components/codefromremote.tsx
+++ src/components/codefromremote.tsx
@@ -7,11 +7,62 @@ import { graphql, StaticQuery, useStaticQuery } from 'gatsby'
 interface Props {
   src: string
   lang: Languages
-  title: string
+  title: string,
+  startAt?: string
+  endAt?: string
 }
 
-const CodeFromRemote = ({ lang, src }: Props) => {
-  const { allRemoteFile } = useStaticQuery(graphql`
+const findLine = (needle: string | undefined, haystack: string[]) => {
+  if (!needle) {
+    return 0
+  }
+
+  const index = haystack.findIndex((s) =>
+    s.indexOf(needle) > -1)
+
+  if (index === -1) {
+    return 0
+  }
+
+  return index
+}
+
+const transform = (
+  { lang, startAt, endAt }: Props
+) => (content: string) => {
+  const lines = content.split('\n')
+  let startIndex = findLine(startAt, lines)
+  let endIndex = findLine(endAt, lines)
+
+  if (startAt) {
+    lines.splice(0, startIndex)
+  }
+
+  if (endAt) {
+    lines.splice(endIndex - startIndex)
+  }
+
+  return Prism.highlight(lines.join('\n'), Prism.languages[lang], lang)
+}
+
+const initialState = (props: Props, { allRemoteFile: { nodes } }: any) => {
+  const { src } = props
+
+  let preloaded = nodes.find(
+    ({ url }: { url: string }) => url === src.split('#')[0]
+  )
+
+  if (!preloaded) {
+    return ''
+  }
+
+  return transform(props)(preloaded.content)
+}
+
+const CodeFromRemote = (props: Props) => {
+  const { lang } = props
+
+  const [content, setContent] = useState(initialState(props, useStaticQuery(graphql`
     {
       allRemoteFile {
         nodes {
@@ -20,49 +71,16 @@ const CodeFromRemote = ({ lang, src }: Props) => {
         }
       }
     }
-  `)
+  `)))
 
-  let preloaded = allRemoteFile.nodes.find(
-    ({ url }: { url: string }) => url === src.split('#')[0]
-  )
-
-  let initialState = ''
-  if (preloaded) {
-    initialState = Prism.highlight(
-      preloaded.content,
-      Prism.languages[lang],
-      lang
-    )
-  }
-
-
-  const location = src
-    .replace('github.com', 'raw.githubusercontent.com')
-    .replace('/blob/', '/')
-
-  const [content, setContent] = useState(initialState)
-  useSSE(() => {
+  useEffect(() => {
+    const location = props.src
+      .replace('github.com', 'raw.githubusercontent.com')
+      .replace('/blob/', '/')
     fetch(location)
       .then((body) => body.text())
-      .then((content) => {
-        // https://github.com/ory/kratos-selfservice-ui-react-native/blob/master/App.tsx#L37-L65
-        const params =
-          location.match(
-            /^https:\/\/raw.githubusercontent.com\/.+#L([0-9]+)-L([0-9]+)$/
-          ) || []
-
-        if (params.length === 3) {
-          const lines = content.split('\n')
-          lines.splice(0, Number(params[1]) - 1)
-          // lines.splice(Number(params[2]) - Number(params[1]) - 1)
-          return lines.join('\n')
-        }
-
-        return content
-      })
-      .then((content) => {
-        setContent(Prism.highlight(content, Prism.languages[lang], lang))
-      })
+      .then(transform(props))
+      .then(setContent)
       .catch(console.error)
   }, [])
 
