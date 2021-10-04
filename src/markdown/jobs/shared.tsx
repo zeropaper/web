ac41ae572306b81aaeef62de58849437bfa98cbf
Patrik <zepatrik@users.noreply.github.com>
2021-10-04 21:41:34 +0200

    feat: de-dupe jobs postings content (#359)
    Co-authored-by: John <jfcurran@users.noreply.github.com>
    Co-authored-by: Vincent <vincent@ory.sh>

---
+++ src/markdown/jobs/shared.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export const About = () => (
+  <>
+    <p>
+      We believe that today's "digital identity" is fundamentally flawed. There
+      are too many profiles, registration screens, passwords, privacy
+      violations, data leaks, phishing attacks, and data mining operations out
+      there. Our vision is to make the internet more secure and private with
+      better software, open-source, and open knowledge.
+    </p>
+  </>
+)
+
+export const Dev = () => (
+  <>
+    <p>
+      Ory is the largest open-source authentication and authorization (login,
+      registration, MFA, permissions, OAuth2, ...) ecosystem and community in
+      the world. We develop open-source software and use it to build a scalable,
+      easy-to-use, and secure cloud platform. You will work on one of the
+      industry's most important code-bases, having a positive impact on hundreds
+      of millions of people worldwide.
+    </p>
+  </>
+)
+
+export const Sales = () => (
+  <>
+    <p>
+      Ory is the largest open-source authentication and authorization (login,
+      registration, MFA, permissions, OAuth2, ...) ecosystem and community in
+      the world. We develop open-source software and use it to build a scalable,
+      easy-to-use, and secure cloud platform. You will talk to customers to
+      understand their biggest problems and tailor solutions to solve them,
+      having a positive impact on hundreds of millions of people worldwide.
+    </p>
+  </>
+)
+
+export const WorkAtOry = () => (
+  <>
+    <p>
+      Ory is an engineering company with a high degree of freedom and autonomy.
+      You will be working with experienced, helpful, and talented peers who all
+      share a deep passion for software.
+    </p>
+    <p>
+      Ory is just starting and there are countless opportunities and constant
+      work in progress. We support each other in our curiosity and creativity
+      which we pursue through thoughtful discussion, transparency, and
+      knowledge-sharing among a diverse set of peers and colleagues. We want to
+      work in a company of warm, inclusive people who treat their colleagues
+      exceptionally well.
+    </p>
+    <p>
+      We are looking for people with passion, curiosity, and integrity. You're
+      encouraged to apply even if your experience does not precisely match the
+      job description. Your skills and curiosity will stand out and set you
+      apart.
+    </p>
+    <p>
+      Interested? Drop us a short introductory email to{' '}
+      <a href="mailto:jobs@ory.sh">jobs@ory.sh</a> and please make sure to
+      include a link to your GitHub profile.
+    </p>
+  </>
+)
