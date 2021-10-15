a83941690c215100fdeeffb1fa1650602bc093df
jfcurran <john@ory.sh>
2021-10-15 12:37:55 +0200

    update blog overview to feature one highlighted post, 3 minor highlighted posts, and all other posts - additional styling needed

--- src/components/layouts/blog/blog-list.tsx
+++ src/components/layouts/blog/blog-list.tsx
@@ -1,24 +1,25 @@
 import cn from 'classnames'
-import { graphql, useStaticQuery } from 'gatsby'
+import {graphql, useStaticQuery} from 'gatsby'
 import React from 'react'
 
 import Button from '../../freestanding/button/button'
 import Container from '../../freestanding/containers/container'
 import Grid from '../../freestanding/containers/grid'
 
-import { AuthorName } from './blog-author'
+import {AuthorName} from './blog-author'
 
 import {
   pb16,
-  pb32,
   pb64,
   pb8
 } from '../../freestanding/utils/padding.module.css'
 import * as styles from './blog-list.module.css'
+import {GatsbyImage, getImage, IGatsbyImageData,} from "gatsby-plugin-image";
 
 export interface PropTypes {
   id: string
   title: string
+  featuredSmall: [string, string, string]
 }
 
 type Edge = {
@@ -31,14 +32,65 @@ type Edge = {
       title: string
       teaser: string
       overline: string
+      banner: IGatsbyImageData
     }
   }
 }
 
-const BlogList = ({ id, title }: PropTypes) => {
+const Post = ({node}: Edge) => {
+  const image = getImage(node.frontmatter.banner)
+  return (<Grid
+      key={node.id}
+      lg={4}
+      md={4}
+      sm={6}
+      xs={12}
+      className={cn(styles.blogItem, styles.itemFlex)}
+    >
+      <Button style={'none'} to={node.frontmatter.path}>
+        {image ? (
+          <GatsbyImage
+            image={image}
+            alt={node.frontmatter.title}/>) : null}
+        <Container
+          className={styles.blogText}
+          justify={'space-between'}
+          alignItems={'stretch'}
+          flexContainer={'column'}
+        >
+          <Container
+            flexContainer={'column'}
+            justify={'start'}
+            alignItems={'start'}
+            className={cn(pb16, styles.minHeight)}
+          >
+            <h2 className={cn('font-overline', pb16)}>
+              <span className={cn('is-themed-primary')}>&gt; </span>
+              {node.frontmatter.overline}
+            </h2>
+            <h3 className={cn('font-h5', pb8)}>
+              {node.frontmatter.title}
+            </h3>
+          </Container>
+          <Container justify={'start'} className={cn(pb16)}>
+            <p className={cn('font-p-smaller')}>
+              <AuthorName
+                className={cn('font-p-smaller')}
+                name={node.frontmatter.author}
+              />{' '}
+              - {node.frontmatter.publishedAt}
+            </p>
+          </Container>
+        </Container>
+      </Button>
+    </Grid>
+  )
+}
+
+const BlogList = ({id, title, featuredSmall}: PropTypes) => {
   const data = useStaticQuery(graphql`
     query {
-      allMdx(
+      allPosts: allMdx(
         filter: {
           fileAbsolutePath: { regex: "/blog/" }
           frontmatter: { published: { ne: false } }
@@ -50,9 +102,15 @@ const BlogList = ({ id, title }: PropTypes) => {
             id
             excerpt(pruneLength: 250)
             frontmatter {
-              seo {
-                title
-                description
+              banner{
+                childImageSharp {
+                  gatsbyImageData(
+                    blurredOptions: {width: 100}
+                    placeholder: BLURRED
+                    transformOptions: {cropFocus: CENTER}
+                    aspectRatio: 1.8
+                  )
+                }
               }
               publishedAt(formatString: "MMMM DD, YYYY")
               author
@@ -66,6 +124,11 @@ const BlogList = ({ id, title }: PropTypes) => {
       }
     }
   `)
+
+  const [featuredPost, ...posts] = (data.allPosts.edges as Edge[])
+  const filterFeaturedSmall = ({node: {frontmatter: {path}}}: Edge) => featuredSmall.indexOf(path) >= 0
+  const featuredSmallPosts = posts.filter(filterFeaturedSmall)
+  const nonFeaturedPosts = posts.filter((edge) => !filterFeaturedSmall(edge))
 
   return (
     <div id={id} className={cn(styles.blogList)}>
@@ -83,54 +146,13 @@ const BlogList = ({ id, title }: PropTypes) => {
             </Grid>
           </Container>
           <Container alignItems={'stretch'} justify={'start'}>
-            {(data.allMdx.edges as Edge[]).map(({ node }) => (
-              <Grid
-                key={node.id}
-                lg={4}
-                md={4}
-                sm={6}
-                xs={12}
-                className={cn(styles.blogItem, styles.itemFlex)}
-              >
-                <Button style={'none'} to={node.frontmatter.path}>
-                  <Container
-                    justify={'space-between'}
-                    alignItems={'stretch'}
-                    flexContainer={'column'}
-                  >
-                    <Container
-                      flexContainer={'column'}
-                      justify={'start'}
-                      alignItems={'start'}
-                      className={cn(pb16, styles.minHeight)}
-                    >
-                      <h2 className={cn('font-overline', pb16)}>
-                        <span className={cn('is-themed-primary')}>&gt; </span>
-                        {node.frontmatter.overline}
-                      </h2>
-                      <h3 className={cn('font-h5', pb8)}>
-                        {node.frontmatter.title}
-                      </h3>
-                    </Container>
-                    <Container justify={'start'} className={cn(pb16)}>
-                      <p className={cn('font-p-smaller')}>
-                        <AuthorName
-                          className={cn('font-p-smaller')}
-                          name={node.frontmatter.author}
-                        />{' '}
-                        - {node.frontmatter.publishedAt}
-                      </p>
-                    </Container>
-                  </Container>
-                  {/* <Container>
-                            <p className={cn('font-p-small', 'is-secondary-text')}>
-                            {node.frontmatter.teaser}{' '}
-                            <span className={'is-semibold'}>Read more</span>
-                            </p>
-                            </Container>*/}
-                </Button>
-              </Grid>
-            ))}
+            <Post {...featuredPost} />
+          </Container>
+          <Container alignItems={'stretch'} justify={'start'}>
+            {featuredSmallPosts.map((edge) => <Post {...edge} />)}
+          </Container>
+          <Container alignItems={'stretch'} justify={'start'}>
+            {nonFeaturedPosts.map((edge) => <Post {...edge} />)}
           </Container>
         </Grid>
       </Container>
