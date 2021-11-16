7a7b99d84c292bdea44193aebd1c1335df62435c
Michiel Leyman <michiel.leyman@gmail.com>
2021-11-16 21:09:38 +0100

    feat: load consent manager

--- src/analytics.ts
+++ src/analytics.ts
@@ -12,7 +12,30 @@ declare global {
   interface Window {
     plausible: any
     _paq: any
+    _iub: any
   }
+}
+
+const loadConsent = () =>
+  new Promise<void>((resolve) => {
+    const existingScript = document.getElementById('iub-consent')
+    if (existingScript) return resolve()
+
+    const script = document.createElement('script')
+    script.src = 'https://cdn.iubenda.com/cons/iubenda_cons.js'
+    script.id = 'iub-consent'
+    document.body.appendChild(script)
+    script.onload = () => resolve()
+  })
+
+export const init = () => {
+  const _iub = window._iub || {}
+  const publicKey = 'Kwz8UykLe0l0hkBIT3qlC2i8uwjFbVzI'
+  _iub.cons_instructions = _iub.cons_instructions || []
+  _iub.cons_instructions.push(['init', { api_key: publicKey }])
+  loadConsent().then(() => {
+    console.log('consent loaded')
+  })
 }
 
 export const trackEvent = (event: IEvent) => {
