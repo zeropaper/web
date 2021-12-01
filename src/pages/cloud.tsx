94d0350783fb05a2e3af12ace561aec87a0c1b1c
vinckr <vincent@ory.sh>
2021-12-01 16:04:06 +0100

    feat: cloud page skeleton

---
+++ src/pages/cloud.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import Adopters from '../components/layouts/adopters/adopters'
+import Coding from '../components/layouts/coding/coding'
+import FeatureContent from '../components/layouts/feature-content/feature-content'
+import Hero from '../components/layouts/hero/hero'
+import Layout from '../components/layouts/layout/layout'
+import Quickstart from '../components/layouts/quickstart/quickstart'
+import Quotes from '../components/layouts/quotes/quotes'
+import SEO from '../components/layouts/seo/seo'
+import Stats from '../components/layouts/stats/stats'
+import * as content from '../page-content/content-index'
+import { sharedAdopters } from '../page-content/shared/content-adopters'
+import { sharedQuotes } from '../page-content/shared/content-quotes'
+import { sharedStats } from '../page-content/shared/content-stats'
+
+const CloudPage = () => (
+  <Layout>
+    <SEO {...content.seo} />
+    <Hero {...content.hero} />
+    <Adopters {...sharedAdopters} />
+    <FeatureContent {...content.featureContentCloud} />
+    <FeatureContent {...content.featureContentUX} />
+    <Coding {...content.coding} />
+    <Stats {...sharedStats} />
+    <Quotes {...sharedQuotes} />
+    <Quickstart {...content.quickstart} />
+  </Layout>
+)
+
+export default CloudPage
